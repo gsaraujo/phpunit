9d1d5f074c0ecff42a42faa670dd2c801f87f818
Gustavo <sasaeucomvoce@hotmail.com>
2020-10-18 12:04:39 -0300

    Full TDD cycle completed (Red,Green,Refactor)

--- tests/Arithmetic/SubtractionTest.php
+++ tests/Arithmetic/SubtractionTest.php
@@ -2,6 +2,7 @@
 
 namespace CodeTests\Arithmetic;
 use PHPUnit\Framework\TestCase;
+use CodeTests\Arithmetic\Subtraction;
 
 /**
  * Some random tests with phpunit (TDD - I started with this class, then the Subtraction class)
@@ -15,5 +16,32 @@ class SubtractionTest extends TestCase
 	public function assertPreConditions()
 	{		
 		$this->assertTrue(class_exists('CodeTests\Arithmetic\Subtraction'));
-	}	
+	}
+
+	public function testTwoNumbersSubtraction()	
+	{
+		$sub = new Subtraction();
+		$sub->setNum1(15);
+		$sub->setNum2(10);
+
+		$this->assertEquals(5,$sub->subtract());
+
+		$sub = new Subtraction();
+		$sub->setNum1(50);
+		$sub->setNum2(30);
+
+		$this->assertEquals(20,$sub->subtract());
+	}
+
+	/**
+	* @expectedException \InvalidArgumentException
+	* @expectedExceptionMessage No parameter
+	**/
+
+	public function testCaseParametersAreEmpty()
+	{
+		$sub = new Subtraction();
+		$sub->setNum1(50);
+		$sub->setNum2();
+	}
 }
